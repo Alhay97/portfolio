dbff69907ce3f4c97b22f1e03a72cd9546315de5
Alhay97 <alhay.7@gmail.com>
2023-02-23 19:40:20 +0400

    done name

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="app">
       <Head>
-        <title>Nikhil Rajput - Portfolio</title>
-        <meta name="description" content="Nikhil Rajput's portfolio developed using Next.js." />
+        <title>Alhai Ahmed Alhameli - Portfolio</title>
+        <meta name="description" content="Alhai Ahmed Alhameli's portfolio developed using Next.js." />
         <meta name="keywords" content="Nixrajput,nixrajput,nix,software engineer,full stack developer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
